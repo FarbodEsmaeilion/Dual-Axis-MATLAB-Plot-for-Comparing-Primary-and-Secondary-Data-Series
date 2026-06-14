2af101576c62fa10aee70a3c74dac0a972778040
Farbod Esmaeilion <farbodesmaeilion@gmail.com>
2026-06-14 09:28:51 +0330

    Add files via upload

---
+++ code fitting.m
@@ -0,0 +1,19 @@
+figure; 
+plot(AC)
+hold on;
+plot(AE);
+%plot(AG);
+
+% Create plot with right y-axis
+yyaxis right
+plot(AB, 'r--')    % Red dashed line
+ylabel('RTE')   % Right y-axis label
+
+
+% Create plot with right y-axis
+yyaxis right
+plot(AD, 'b--')    % Red dashed line
+
+% Create plot with right y-axis
+%yyaxis right
+%plot(AF, 'G--')    % Red dashed line
